3490bbc1f47f0d3866781aa3008d96beec4a5eba
Ivan <ivan.ab001@yandex.ru>
2018-09-24 16:58:09 +0300

    "No changes, first commit"

--- Messenger/AppDelegate.swift
+++ Messenger/AppDelegate.swift
@@ -13,34 +13,72 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var prev_State = "'Not running'"
+    
+    func print_State(curr_State: String, method: String){
+        if self.prev_State == curr_State {
+            print("Application will remain \(curr_State): \(method)")
+        }else {
+            print("Application moved from \(self.prev_State) to \(curr_State): \(method)")
+            self.prev_State = curr_State
+        }
+    }
+    
+    func get_State(method: String) {
+        switch UIApplication.shared.applicationState {
+        case .active:
+            print_State(curr_State: "'Active'", method: method)
+        case .inactive:
+            print_State(curr_State: "'Inactive'", method: method)
+        case .background:
+            print_State(curr_State: "'Background'", method: method)
+        }
+    }
 
-
+    
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+        switch UIApplication.shared.applicationState {
+        case .active: self.prev_State = "'Active'"
+        case .inactive: self.prev_State = "'Inactive'"
+        case .background: self.prev_State = "'Background'"
+        }
+        print("Application moved from 'Not running' to 'Inactive': application(_:willFinishLaunchingWithOptions:)")
+        return true
+    }
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        //UIApplication.shared.applicationState
+        get_State(method: #function)
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        get_State(method: #function)
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        get_State(method: #function)
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        get_State(method: #function)
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        get_State(method: #function)
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         // Saves changes in the application's managed object context before the application terminates.
+        get_State(method: #function)
         self.saveContext()
     }
 
--- Messenger/ViewController.swift
+++ Messenger/ViewController.swift
@@ -8,12 +8,51 @@
 
 import UIKit
 
+public func print(_ items: Any..., separator: String = " ", terminator: String = "\n") {
+    #if DEBUG
+        let output = items.map { "\($0)" }.joined(separator: separator)
+        Swift.print(output, terminator: terminator)
+    #endif
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(#function)
         // Do any additional setup after loading the view, typically from a nib.
     }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print(#function)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print(#function)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        print(#function)
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        print(#function)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print(#function)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
